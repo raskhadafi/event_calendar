8dcecd6e961f01e8b4145495f1b056494e62f652
Dieter <coder2000@gmail.com>
2010-04-08 03:04:38 -0500

    Removed railtie

--- lib/event_calendar.rb
+++ lib/event_calendar.rb
@@ -1,4 +1,14 @@
-module EventCalendar
-  require 'lib/event_calendar/railtie' if defined?(Rails)
+require 'active_record'
+require 'action_view'
+
+require 'event_calendar/plugin_methods'
+require 'event_calendar/calendar_helper'
+
+if defined?(ActiveRecord::Base)
+  ActiveRecord::Base.extend EventCalendar::PluginMethods
+end
+
+if defined?(ActionView::Base)
+  ActionView::Base.send :include, EventCalendar::CalendarHelper
 end
 
